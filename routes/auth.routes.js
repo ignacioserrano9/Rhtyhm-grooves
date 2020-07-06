d924045aaae6e7e3ea31a09a4f96ab65f1731e83
ignacioserrano9 <serranoi0910@gmail.com>
2020-07-06 19:39:02 +0200

    stoped at adding discs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -15,32 +15,48 @@ const bcryptSalt = 10
 router.get("/signup", (req, res) => res.render("auth/signup"))
 router.post("/signup", (req, res, next) => {
 
-    const { username, password } = req.body
+    const {
+        username,
+        password
+    } = req.body
 
     if (!username || !password) {
-        res.render("auth/signup", { errorMsg: "Rellena el usuario y la contraseña" })
+        res.render("auth/signup", {
+            errorMsg: "Rellena el usuario y la contraseña"
+        })
         return
     }
 
-    User.findOne({ username })
+    User.findOne({
+            username
+        })
         .then(user => {
             if (user) {
-                res.render("auth/signup", { errorMsg: "El usuario ya existe en la BBDD" })
+                res.render("auth/signup", {
+                    errorMsg: "El usuario ya existe en la BBDD"
+                })
                 return
             }
             const salt = bcrypt.genSaltSync(bcryptSalt)
             const hashPass = bcrypt.hashSync(password, salt)
 
-            User.create({ username, password: hashPass })
+            User.create({
+                    username,
+                    password: hashPass
+                })
                 .then(() => res.redirect("/"))
-                .catch(() => res.render("auth/signup", { errorMsg: "No se pudo crear el usuario" }))
+                .catch(() => res.render("auth/signup", {
+                    errorMsg: "No se pudo crear el usuario"
+                }))
         })
         .catch(error => next(error))
 })
 
 
 // User login
-router.get('/login', (req, res) => res.render('auth/login', { "errorMsg": req.flash("error") }))
+router.get('/login', (req, res) => res.render('auth/login', {
+    "errorMsg": req.flash("error")
+}))
 router.post('/login', passport.authenticate("local", {
     successRedirect: "/profile",
     failureRedirect: "/login",
@@ -51,8 +67,22 @@ router.post('/login', passport.authenticate("local", {
 
 // User profile
 
-router.get('/profile', ensureLogin.ensureLoggedIn(), (req, res) => res.render('user/profile', { user: req.user }))
-console.log(User)
+router.get('/profile', ensureLogin.ensureLoggedIn(), (req, res) => {
+    User
+        .findOne(req.params.id)
+        .then((data) => {
+            console.log(data)
+            res.render('user/profile', {
+                user: req.user
+            })
+
+        })
+        .catch((error) => {
+            console.warn('Oops, something went wrong!', error)
+        })
+})
+
+
 router.post('/profile', passport.authenticate("local", {
     successRedirect: "/profile",
     failureRedirect: "/login",
@@ -65,7 +95,7 @@ router.post('/profile', passport.authenticate("local", {
 // User logout
 router.get("/logout", (req, res) => {
     req.logout()
-    res.redirect("/login")
+    res.redirect("/")
 })
 
 module.exports = router
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,89 +2,108 @@ const express = require('express')
 const router = express.Router()
 const Disk = require('../models/disk.model')
 const User = require('../models/user.model')
+const passport = require("passport")
+const ensureLogin = require("connect-ensure-login");
+
+
 
 const Discojs = require('discojs')
 const client = new Discojs({
-    consumerKey: process.env.USER_KEY,
-    consumerSecret:process.env.USER_SECRET
+  consumerKey: process.env.USER_KEY,
+  consumerSecret: process.env.USER_SECRET
 
 })
- 
+//Record search
+
 router.get("/record-search", (req, res) => {
-   
-client
-  .searchRelease(req.query.record)
-  .then((data) => {
-      //console.log(data)
-    res.render("disk/search", { data });
-    
-  })
-  .catch((error) => {
-    console.warn('Oops, something went wrong!', error)
-  })
 
-})
-router.get("/record/:id", (req, res) => {
-   //console.log(typeof(req.params.id))
-   let releaseId = parseInt(req.params.id)
   client
-    .getRelease(releaseId)
+    .searchRelease(req.query.record)
     .then((data) => {
-        console.log(data)
-      res.render("disk/record", data );
+      //console.log(data)
+      res.render("disk/search", {
+        data
+      });
 
     })
     .catch((error) => {
       console.warn('Oops, something went wrong!', error)
     })
+
+})
+
+//Record view
+router.get("/record/:id", (req, res) => {
+  //console.log(typeof(req.params.id))
+  let releaseId = parseInt(req.params.id)
+  client
+    .getRelease(releaseId)
+    .then((data) => {
+      //console.log(data)
+      res.render("disk/record", data);
+
+    })
+    .catch((error) => {
+      console.warn('Oops, something went wrong!', error)
+    })
+
+})
+router.post("/record/addcollection/:id", (req, res) => {
+  let releaseId = parseInt(req.params.id)
+  let userdata = User.findById(req.params.id)
+  let record = client.getRelease(releaseId)
+  //console.log(typeof(req.params.id))
+  Promise.all([userdata, record])
   
+  .then(data => {
+  console.log(data[1])
+  Disk.insertOne({record: data[1]})
+
+  .catch((error) => {
+    console.warn('Oops, something went wrong!', error)
   })
-  router.get("/record/addcollection/:id", (req, res) => {
-    //console.log(typeof(req.params.id))
-    let releaseId = parseInt(req.params.id)
-   client
-     .getRelease(releaseId)
-     .then((data) => {
-         console.log(data)
+.then(data=> {
+  User.findOneAndUpdate({record: data[1].id}, {record: data[2].id} )
 
-       res.redirect("/user/collection", data );
- 
-     })
-     .catch((error) => {
-       console.warn('Oops, something went wrong!', error)
-     })
-   
-   })
- 
-
-module.exports = router
+})
+res.redirect("/user/collection", data);
+})
+})
 
 
+    
 
 
 
 // --- COLLECTION ---
 
-//router.get('/user/_id/collection', (req, res) => res.render('../views/user/collection.hbs'))
+router.get('/user/:_id/collection', ensureLogin.ensureLoggedIn(), (req, res) => {
+  User
+    .findOne(req.params.id)
+    .then((data) => {
+      //console.log(data)
+      res.render('user/collection', data)
 
-//router.post('/users/{username}/collection', (req,res) )
-
-// router.get('/collection', (req, res) => {
-//   User.find()
-//     .then(allRecords => res.render('/user/collection', {
-//       records: allRecords
-//     }))
-//     .catch(err => console.log('error', err))
-// })
-
-
-
-// --- WISHLIST ---
-
-// router.get('/user/_id/wishlist', (req, res) => res.render('../views/user/wishlist.hbs'))
+    })
+    .catch((error) => {
+      console.warn('Oops, something went wrong!', error)
+    })
+})
 
 
 
 
+router.get('/user/:_id/wishlist', ensureLogin.ensureLoggedIn(), (req, res) => {
+User
+    .findOne(req.params.id)
+    .then((data) => {
+      //console.log(data)
+      res.render('user/wishlist', data)
 
-// module.exports = router
+    })
+    .catch((error) => {
+      console.warn('Oops, something went wrong!', error)
+    })
+})
+
+module.exports = router
