1c527d02cbc7f0ed3a3d7ddd91ebb7a25b4129b1
Rodrigo <user@Rodrigos-MacBook-Pro.local>
2020-07-08 14:46:31 +0200

    vistas finalizadas

--- models/disk.model.js
+++ models/disk.model.js
@@ -2,12 +2,17 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const diskSchema = new mongoose.Schema({
-    DiscId: Number,
-     
-    image: String,
-    
-    title: String,
+    discId: {
+        type: String,   
+    },
+    image: {
+        type: String,  
+    },
+    title: {
+        type: String,   
+    },
     recordOwner: [{type: Schema.Types.ObjectId, ref: 'User'}],
+
     wishlistOwner: [{type: Schema.Types.ObjectId, ref: 'User'}],
     },{
         timestamps: true
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,159 +1,89 @@
 const express = require('express')
-const mongoose = require("mongoose")
 const router = express.Router()
 const Disk = require('../models/disk.model')
 const User = require('../models/user.model')
-const passport = require("passport")
 const ensureLogin = require("connect-ensure-login");
-
-
-
 const Discojs = require('discojs')
 const { populate } = require('../models/disk.model')
+
 const client = new Discojs({
   consumerKey: process.env.USER_KEY,
   consumerSecret: process.env.USER_SECRET
-
 })
 
-
-
-
-
-//Record search
-
-router.get("/record-search", (req, res) => {
-
+router.get("/record-search", (req, res, next) => {
   client
     .searchRelease(req.query.record)
-    .then((data) => {
-      //console.log(data)
-      res.render("disk/search", {
-        data
-      });
-
-    })
-    .catch((error) => {
-      console.warn('Oops, something went wrong!', error)
-    })
-
+    .then(data => res.render("disk/search", { data }))
+    .catch(err => next(new Error(err)))
 })
 
-//Record view
-router.get("/record/:id",ensureLogin.ensureLoggedIn(), (req, res) => {
-  //console.log(typeof(req.params.id))
+router.get("/record/:id", ensureLogin.ensureLoggedIn(), (req, res, next) => {
   let releaseId = parseInt(req.params.id)
   client
     .getRelease(releaseId)
-    .then((data) => {
-     // console.log(data)
-      res.render("disk/record", data)
-      //console.log(user)
-    })
-    .catch((error) => {
-      console.warn('Oops, something went wrong!', error)
-    })
-
+    .then((data) => (res.render("disk/record", data)))
+    .catch(err => next(new Error(err)))
 })
 
-
-
-router.post("/record/addcollection",ensureLogin.ensureLoggedIn(),(req, res) => {
-  
-  const { DiscId, image, title } = req.body
+router.post("/record/addcollection", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  const { discId, image, title } = req.body
   const recordOwner = req.user._id
   Disk
-  .create({ DiscId, image, title, recordOwner })
-  .then(() => res.redirect('/user/:_id/collection'))
-  .catch((error) => {
-    console.warn('Oops, something went wrong!', error)
-  
-  })
+    .create({ discId, image, title, recordOwner })
+    .then(() => res.redirect('/user/:_id/collection'))
+    .catch(err => next(new Error(err)))
 })
 
-  router.post("/record/addwishlist",ensureLogin.ensureLoggedIn(),(req, res) => {
-  
-    const { DiscId, image, title } = req.body
-    const wishlistOwner = req.user._id
-    Disk
+router.post("/record/addwishlist", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  const { DiscId, image, title } = req.body
+  const wishlistOwner = req.user._id
+  Disk
     .create({ DiscId, image, title, wishlistOwner })
     .then(() => res.redirect('/user/:_id/wishlist'))
-    .catch((error) => {
-      console.warn('Oops, something went wrong!', error)
-    
-    })
-  })
+    .catch(err => next(new Error(err)))
+})
 
-
-// --- COLLECTION ---
-
-router.get('/user/:_id/collection', ensureLogin.ensureLoggedIn(), (req, res) => {
-  
+router.get('/user/:_id/collection', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   const userId = User.findById(req.user._id)
-  const diskInfo = Disk.find({recordOwner: req.user._id})
-  
-  Promise.all([userId, diskInfo]) 
-  .then(records => {
-    //console.log( records)
-      res.render('user/collection', { user: records[0], disk: records[1] }) 
-  })
-  .catch(err => console.log("Error en la BBDD", err))
+  const diskInfo = Disk.find({ recordOwner: req.user._id })
+
+  Promise.all([userId, diskInfo])
+    .then(records => res.render('user/collection', { user: records[0], disk: records[1] }))
+    .catch(err => next(new Error(err)))
 })
 
-// --- Wishlist---
-
-router.get('/user/:_id/wishlist', ensureLogin.ensureLoggedIn(), (req, res) => {
+router.get('/user/:_id/wishlist', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   const userId = User.findById(req.user._id)
-  const diskInfo = Disk.find({wishlistOwner: req.user._id})
-  
-  Promise.all([userId, diskInfo]) 
-  .then(records => {
-    //console.log( records)
-      res.render('user/wishlist', { user: records[0], disk: records[1] }) 
-  })
-  .catch(err => console.log("Error en la BBDD", err))
+  const diskInfo = Disk.find({ wishlistOwner: req.user._id })
+
+  Promise.all([userId, diskInfo])
+    .then(records => res.render('user/wishlist', { user: records[0], disk: records[1] }))
+    .catch(err => next(new Error(err)))
 })
 
-
-router.get('/profile/:id', ensureLogin.ensureLoggedIn(), (req, res) => {
+router.get('/profile/:id', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   User
-      .findOne(req.params.id)
-      .then((data) => {
-          //console.log("ddddddd",data)
-          res.render('user/profile', {
-              user: req.user
-          })
-
-      })
-      .catch((error) => {
-          console.warn('Oops, something went wrong!', error)
-      })
+    .findOne(req.params.id)
+    .then(data => res.render('user/profile', {user: req.user}))
+    .catch(err => next(new Error(err)))
 })
 
-
-//comunity
-router.get('/community', ensureLogin.ensureLoggedIn(), (req, res) => {
-
+router.get('/community', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   User
-  .find()
-  .then(users => {
-    //console.log(users)
-    res.render("user/community", {users})
-  }) 
-  .catch(err => console.log("Error en la BBDD", err))
-  })
+    .find()
+    .then(users => res.render("user/community", { users }))
+    .catch(err => next(new Error(err)))
+})
 
-router.get('/community/:id', ensureLogin.ensureLoggedIn(), (req, res) => {
+router.get('/community/:id', ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  const userId = User.findById(req.params.id)
+  const diskInfoRec = Disk.find({ recordOwner: req.params.id })
+  const diskInfoWis = Disk.find({ wishlistOwner: req.params.id })
+
+  Promise.all([userId, diskInfoRec, diskInfoWis])
+    .then(records => res.render('commprofile', { user: records[0], vynil: records[1], wish: records[2] }))
+    .catch(err => next(new Error(err)))
+  })
   
-  const userId =  User.findById(req.params.id)
-  const diskInfoRec = Disk.find({recordOwner:req.params.id })
-  const diskInfoWis = Disk.find({wishlistOwner:req.params.id })
-  
-        Promise.all([userId, diskInfoRec, diskInfoWis]) 
-        .then(records => {
-          console.log( records)
-            res.render('commprofile', { user: records[0], vynil: records[1], wish: records[2]}) 
-        })
-        .catch(err => console.log("Error en la BBDD", err))
-      })
 module.exports = router
