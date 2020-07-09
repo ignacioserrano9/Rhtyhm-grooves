71555b8da2fe81cd151390bd0fb69425f0df15e3
ignacioserrano9 <serranoi0910@gmail.com>
2020-07-09 12:17:46 +0200

    upload to heroku

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -3,11 +3,6 @@ const router = express.Router()
 const multer = require('multer')
 const User = require('../models/user.model')
 const Picture = require('../models/picture.model')
-
-
-
-// CDN file upoloads routs
-
 const cloudUploader = require('../configs/cloudinary.config')
 
 router.get('/user/:_id/edit', (req, res, next) => {
@@ -16,14 +11,9 @@ router.get('/user/:_id/edit', (req, res, next) => {
     .catch(err => next(new Error(err)))
 })
 router.post('/user/:_id/edit', cloudUploader.single('imageFile'), (req, res, next) => {
-
-    console.log('Multer, en combinación con Cloudinary, crea este req.file:', req.file)
-
-User.findByIdAndUpdate({_id: req.user.id},{avatar: req.file.path})
-
+    User.findByIdAndUpdate({_id: req.user.id},{avatar: req.file.path})
     .then(user => res.redirect('/profile'))
     .catch(err => next(new Error(err)))
-
 })
 
 
